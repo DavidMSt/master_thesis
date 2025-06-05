65a826f192fdd48f3b6a4562163dc818e841ba8c
David Stoll <davidstoll@MacBook-Pro-von-David-5.local>
2025-06-05 17:34:47 +0200

    initial commit

---
+++ testbed/david_thesis/main.py
@@ -0,0 +1,29 @@
+import numpy as np
+from typing import Callable
+
+# TODO: install auto docstring
+
+class TaskAssignment():
+   method: str # method being used for task assignment
+   cost_function: Callable # Cost function we try to optimize
+
+    def lsap(C:np.array, S: np.array):
+        np.sum
+      
+
+class Task():
+   def __init__(self):
+      pass
+   
+class Agent():
+    position : np.array
+    assigned_task: Task
+
+    def __init__(self, position):
+       self.position = position
+
+def main():
+   ...
+
+if __name__ == "__main__":
+ main()
